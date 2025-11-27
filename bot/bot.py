91024209c8b4a42ac7db4b01ad172ed5ff218a96
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2025-11-26 23:19:02 -0300

    fix(bot): Update bot to use new comandas and functions

--- bot/bot.py
+++ bot/bot.py
@@ -2,8 +2,8 @@ import logging as log
 import os
 import sys
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CommandHandler, Updater, CallbackQueryHandler
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackQueryHandler, ApplicationBuilder, ContextTypes, Application
 
 from bot.menu import main_menu_keyboard, lottery_menu_keyboard
 from features.lottery.loteria import get_result
@@ -13,56 +13,61 @@ from features.messages.messages import main_menu_message, continue_menu_message,
 log.basicConfig(level=log.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = log.getLogger(__name__)
 
-mode = os.getenv("MODE")
-token = os.getenv("TOKEN")
+MODE = os.getenv("MODE")
+TOKEN = os.getenv("TOKEN")
+PORT = os.getenv("PORT", "8443")
+HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 
 
-# def start(update: Update, context: CallbackContext):
-def start(update, context):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.info("Boot Iniciado")
-    context.bot.send_message(chat_id=update.effective_chat.id, text=main_menu_message(),
-                             reply_markup=main_menu_keyboard())
-    # update.message.reply_text(main_menu_message(), reply_markup=main_menu_keyboard())
+    """Sends a message with three two buttons attached."""
+    await update.message.reply_text(main_menu_message(), reply_markup=main_menu_keyboard("get_id", "list_lottery"))
 
 
-def start_continue(update, context):
-    logger.info("Retorno do menu")
-    context.bot.send_message(chat_id=update.effective_chat.id, text=continue_menu_message(),
-                             reply_markup=main_menu_keyboard())
+async def start_continue(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.info("Retorno do menu: Continuar")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=continue_menu_message(),
+                                   reply_markup=main_menu_keyboard("get_id", "list_lottery"))
 
 
-def get_id(update, context):
+async def get_id(update: Update, context: ContextTypes.DEFAULT_TYPEt):
     logger.info("Retorna o id do Usuario")
-    query = update.callback_query
-    response_message = query.message.chat_id
-    query.edit_message_text(response_message)
-    start_continue(update, context)
+    await update.callback_query.edit_message_text(
+        text="Hi! Your chat id is: {}".format(update.effective_chat.id))
+    # await context.bot.send_message(
+    #     chat_id=update.effective_chat.id,
+    #     text="Hi! Your chat id is: {}".format(update.effective_chat.id)
+    # )
+    # After the initial menu, ask again what the user wants to do
+    await start_continue(update, context)
 
 
-def get_lottery(update, context):
+async def get_lottery(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     logger.info("Chamou a loteria {}".format(query.data))
     dados = get_result(query.data)
-    query.edit_message_text(text="Concurso nª {} \nDezenas sorteadas: {}".format(dados[0], dados[1:len(dados) + 1]))
-    start_continue(update, context)
+    await query.edit_message_text(
+        text="Concurso nª {} \nDezenas sorteadas: {}".format(dados[0], dados[1:len(dados) + 1]))
+    await start_continue(update, context)
 
 
-def list_lottery(update, context):
-    query = update.callback_query
-    context.bot.send_message(chat_id=query.message.chat_id,
-                             text=list_lottery_message(),
-                             reply_markup=lottery_menu_keyboard())
+async def list_lottery(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.callback_query.edit_message_text(
+        text=list_lottery_message(), reply_markup=lottery_menu_keyboard())
 
 
-def run(updater):
-    if mode == "dev":
-        updater.start_polling()
-    elif mode == "prd":
-        PORT = int(os.environ.get("PORT", "88"))
-        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+def run(application: Application):
+    if MODE == "dev":
+        application.run_polling()
+    elif MODE == "prd":
         # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
-        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=token)
-        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, token))
+        application.run_webhook(
+            listen="0.0.0.0",
+            port=int(PORT),
+            secret_token=TOKEN,
+            webhook_url="https://{}.herokuapp.com/".format(HEROKU_APP_NAME)
+        )
     else:
         logger.error("No MODE specified!")
         sys.exit(1)
@@ -70,11 +75,10 @@ def run(updater):
 
 def main_bot():
     logger.info("Starting bot")
-    updater = Updater(token)
-    dispatcher = updater.dispatcher
-    run(updater)
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CallbackQueryHandler(get_id, pattern='get_id'))
-    dispatcher.add_handler(CallbackQueryHandler(list_lottery, pattern='list_lottery'))
-    dispatcher.add_handler(CallbackQueryHandler(get_lottery))
-    updater.idle()
+    application = ApplicationBuilder().token(TOKEN).build()
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CallbackQueryHandler(get_id, pattern="get_id"))
+    application.add_handler(CallbackQueryHandler(list_lottery, pattern="list_lottery"))
+    application.add_handler(CallbackQueryHandler(get_lottery,
+                                                 pattern="^(diadesorte|duplasena|lotofacil|lotomania|megasena|quina|timemania|supersete|milionaria)$"))
+    run(application)
